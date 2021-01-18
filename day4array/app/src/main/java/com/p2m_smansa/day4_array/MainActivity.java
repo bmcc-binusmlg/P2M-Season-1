b1c6b277d3f86757724e08f96fe205555999e134
hafizhMo <hafizh.hizb@gmail.com>
2021-01-18 20:50:06 +0700

    pertemuan ke 4 - Array

---
+++ day4array/app/src/main/java/com/p2m_smansa/day4_array/MainActivity.java
@@ -0,0 +1,14 @@
+package com.p2m_smansa.day4_array;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
