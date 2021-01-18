7a10406a55c68d225f297dfc5db744ad15814872
hafizhMo <hafizh.hizb@gmail.com>
2021-01-18 20:44:45 +0700

    Pertemuan ke 2 - Layouting

---
+++ day2-layouting/app/src/main/java/com/p2m_smansa/day2_layouting/MainActivity.java
@@ -0,0 +1,14 @@
+package com.p2m_smansa.day1_layouting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
