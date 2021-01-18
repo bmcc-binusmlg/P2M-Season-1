dec2cc32738aa32c507b1b3613f4541bbf264848
hafizhMo <hafizh.hizb@gmail.com>
2021-01-18 20:50:25 +0700

    Pertemuan ke 5 & 6 - Final Project

---
+++ day56finalproject/app/src/main/java/com/p2m_smansa/day5_6_final_project/MainActivity.java
@@ -0,0 +1,14 @@
+package com.p2m_smansa.day5_6_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
