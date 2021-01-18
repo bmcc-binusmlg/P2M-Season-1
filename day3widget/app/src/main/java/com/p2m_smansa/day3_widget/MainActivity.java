04ba30ae05a4e1bfd2fad8bcb8c408c52aa56ba9
hafizhMo <hafizh.hizb@gmail.com>
2021-01-18 20:49:32 +0700

    Pertemuan ke 3 - Widget: TextView, ImageView dan Button

---
+++ day3widget/app/src/main/java/com/p2m_smansa/day3_widget/MainActivity.java
@@ -0,0 +1,14 @@
+package com.p2m_smansa.day3_widget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
